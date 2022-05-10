cdf5e8d5b2b04dacf78b68834bcedba7ff7da904
makak22 <43416202+makak22@users.noreply.github.com>
2022-05-10 10:03:48 +0000

    update2

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Letoli</title>
         <meta name="description" content="Le dico du Boma" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
