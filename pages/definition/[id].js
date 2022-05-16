4f1983aa6cb2a874d24934aa09356e37d3b36ff7
makak22 <43416202+makak22@users.noreply.github.com>
2022-05-16 08:14:27 +0000

    update 0.89

--- pages/definition/[id].js
+++ pages/definition/[id].js
@@ -1,45 +1,83 @@
-import { useRouter } from 'next/router';
+import { toli } from '../../public/data/ladatabase';
 import React from 'react'
-import words from "../../public/data/database.json"
 
-function Definitions() {
 
-    const router = useRouter();
-    React.useEffect(() => {
-        if (router.isReady) {
-      // Code using query
-      const aa = router.query.id;
-        console.log(router.query.id);
-        }
-    }, [router.isReady]);
-
-    const idNum = 0;
-    return (
-        <div className="defContainer">
-
-            <div className="defHeader">
-              <h1 className="defDetails defDef ">{words.data[idNum].attributes.mot}</h1>
-            </div>
-            
-          <div className="defMot">  
-            <div>
-              <h1 className="defTitle">Définition</h1>
-              <h3 className="defDetails defMain">{words.data[idNum].attributes.definition[1]}</h3>
-              <h3 className="defDetails defEx">{words.data[idNum].attributes.definitionEx[1]}</h3>
-            </div>
-            <div>
-              <h1 className="defTitle">Synonymes</h1>
-              <h3 className="defDetails">{words.data[idNum].attributes.synonymes[1]}</h3>
-            </div>
-            <div>
-              <h1 className="defTitle">Expression</h1>
-              <h3 className="defDetails">{words.data[idNum].attributes.expression[1]}</h3>
-            </div>
-          </div>  
-            
-        </div>
-        
-    )
+export const getStaticProps = async ({params}) => {
+    const tolis = toli.filter(mot => mot.id.toString() === params.id)
+    return {
+        props: {
+            mot: tolis[0]
+        },
+    }
 }
 
-export default Definitions;
+export const getStaticPaths = async() => {
+    const paths = toli.map(toli=>({
+        params: {id: toli.id.toString()},
+    }))
+    return{ paths, fallback: false}
+}
+
+export default ({mot}) =>(
+
+    <div className="defContainer">
+
+    <div className="defHeader">
+      <h1 className="defDetails defDef ">{mot.attributes.mot}</h1>
+    </div>
+    
+    <div className="defMot">  
+            <div>
+                <h1 className="defTitle">Définition</h1>
+                <h3 className="defDetails defMain">{mot.attributes.definition[1]}</h3>
+                <h3 className="defDetails defEx">{mot.attributes.definitionEx[1]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Synonymes</h1>
+                <h3 className="defDetails">{mot.attributes.synonymes[1]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Expression</h1>
+                <h3 className="defDetails">{mot.attributes.expression[1]}</h3>
+            </div>
+          
+    </div>
+    
+        {mot.attributes.definition[2] != null && 
+    
+        <div className="defMot">
+            <div>
+                <h1 className="defTitle">Définition</h1>
+                <h3 className="defDetails defMain">{mot.attributes.definition[2]}</h3>
+                <h3 className="defDetails defEx">{mot.attributes.definitionEx[2]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Synonymes</h1>
+                <h3 className="defDetails">{mot.attributes.synonymes[2]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Expression</h1>
+                <h3 className="defDetails">{mot.attributes.expression[2]}</h3>
+            </div>
+        </div> 
+        }
+        {mot.attributes.definition[3] != null && 
+        <div className="defMot">
+            <div>
+                <h1 className="defTitle">Définition</h1>
+                <h3 className="defDetails defMain">{mot.attributes.definition[3]}</h3>
+                <h3 className="defDetails defEx">{mot.attributes.definitionEx[3]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Synonymes</h1>
+                <h3 className="defDetails">{mot.attributes.synonymes[3]}</h3>
+            </div>
+            <div>
+                <h1 className="defTitle">Expression</h1>
+                <h3 className="defDetails">{mot.attributes.expression[3]}</h3>
+            </div>
+        </div>
+        } 
+    </div>        
+
+)
---
+++ public/data/ladatabase.js
@@ -0,0 +1,118 @@
+export const toli = [
+    {
+        "id": 1,
+        "attributes": {
+          "mot": "popoh",
+          "createdAt": "2022-05-10T20:02:39.520Z",
+          "updatedAt": "2022-05-10T20:17:16.252Z",
+          "publishedAt": "2022-05-10T20:02:41.159Z",
+          "definition": {
+            "1": "Used to insult or degrade an opposing opinion, labelling it as average or poor quality",
+            "2":" 2Used to insult or degrade an opposing opinion, labelling it as average or poor quality",
+            "3":" 3Used to insult or degrade an opposing opinion, labelling it as average or poor quality"
+          },
+          "definitionEx": {
+            "1":"we waited on that album for 6 months but it was mid",
+            "2":"2we waited on that album for 6 months but it was mid",
+            "3":"3we waited on that album for 6 months but it was mid"
+          },
+          "synonymes": {
+            "1":"lol, ksa, lopoa",
+            "2":"2lol, ksa, lopoa",
+            "3":"3lol, ksa, lopoa"
+          },
+          "expression": {
+            "1":"je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since",
+            "2":"2je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since",
+            "3":"3je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since"
+          }
+        }
+      },
+      {
+        "id": 2,
+        "attributes": {
+          "mot": "vrai",
+          "createdAt": "2022-05-10T20:02:39.520Z",
+          "updatedAt": "2022-05-10T20:17:16.252Z",
+          "publishedAt": "2022-05-10T20:02:41.159Z",
+          "definition": {
+            "1": "Used to insult or degrade an opposing opinion, labelling it as average or poor quality",
+            "2":null,
+            "3":null
+          },
+          "definitionEx": {
+            "1":"we waited on that album for 6 months but it was mid",
+            "2":null,
+            "3":null
+          },
+          "synonymes": {
+            "1":"lol, ksa, lopoa",
+            "2":null,
+            "3":null
+          },
+          "expression": {
+            "1":"je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since",
+            "2":null,
+            "3": null
+          }
+        }
+      },
+      {
+        "id": 3,
+        "attributes": {
+          "mot": "wanda",
+          "createdAt": "2022-05-10T20:02:39.520Z",
+          "updatedAt": "2022-05-10T20:17:16.252Z",
+          "publishedAt": "2022-05-10T20:02:41.159Z",
+          "definition": {
+            "1": "Used to insult or degrade an opposing opinion, labelling it as average or poor quality",
+            "2":" 2Used to insult or degrade an opposing opinion, labelling it as average or poor quality",
+            "3": null
+          },
+          "definitionEx": {
+            "1":"we waited on that album for 6 months but it was mid",
+            "2":"2we waited on that album for 6 months but it was mid",
+            "3":null
+          },
+          "synonymes": {
+            "1":"lol, ksa, lopoa",
+            "2":"2lol, ksa, lopoa",
+            "3":"3lol, ksa, lopoa"
+          },
+          "expression": {
+            "1":"je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since",
+            "2":"2je wanda sur le frangin al.\nohh popoh lw reeel ??\nmani was toi quel since",
+            "3":null
+          }
+        }
+      },
+      {
+        "id": 4,
+        "attributes": {
+          "mot": "Badeconiser",
+          "createdAt": "2022-05-10T20:02:39.520Z",
+          "updatedAt": "2022-05-10T20:17:16.252Z",
+          "publishedAt": "2022-05-10T20:02:41.159Z",
+          "definition": {
+            "1": "Processus par lequel une enquête est menée pour faire la lumière sur une situation afin de remettre les choses en ordre.",
+            "2":null,
+            "3": null
+          },
+          "definitionEx": {
+            "1":"Ce petit semble avoir tort dans ce qu'il a dit, il mérite d'être bodeconisé.",
+            "2":null,
+            "3":null
+          },
+          "synonymes": {
+            "1":"sicia",
+            "2":null,
+            "3":null
+          },
+          "expression": {
+            "1":"ce samedi tu vas te faire badeconiser",
+            "2":null,
+            "3":null
+          }
+        }
+      }
+]
