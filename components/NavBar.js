69bf39cbef6e46ec504214c8d8723e10fb9eacdf
makak22 <43416202+makak22@users.noreply.github.com>
2022-05-18 10:51:49 +0000

    version 1.0

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,23 @@
 import React from "react"
 import Link from "next/link"
+import { toli } from '../public/data/ladatabase';
 
 function NavBar() {
+    const mots = toli
+    const [searchValue, setSearchValue] = React.useState("");
+    
+    const filteredData = mots.filter((item) => {
+        return item.attributes.mot.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0} )
+    
+    const lesMots = filteredData.sort((a, b) => a.attributes.mot.localeCompare(b.attributes.mot))
+    const toggle = () => setSearchValue("");
     return (
-    <div className="containe">
+    <div>
+        <div className="containe">
         <div className="content">
             <nav className="navBar">
                 <Link href="/">
-                    <a>LETOLI</a>
+                    <a onClick={toggle}>LETOLI</a>
                 </Link>
             </nav>   
                 <div>
@@ -16,9 +26,30 @@ function NavBar() {
                         placeholder="Recherche"
                         title="Search bar"
                         type="text"
+                        onChange={({ target: { value } }) => setSearchValue(value)}
+                        value={searchValue}
+                        
                     />
                 </div>
-         </div>       
+        </div>       
+        </div>
+
+    {searchValue.length != 0 &&(
+        <div className='navContainer'>
+            <div className='results' >
+                {lesMots.slice(0, 10).map((mot) =>(
+
+                    <div key={mot.id}>
+                        <Link href={`/definition/${mot.id}`}>
+                            <h3 onClick={toggle}>{mot.attributes.mot}</h3>
+                        </Link>
+                    </div>
+                    ))}
+                {lesMots.length != 0 ? "" : <h3>Aucun resultat</h3> }
+        
+            </div>
+        </div>
+    )}
     </div>
     )
 }
