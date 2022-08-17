1339368c37f5a1a65689941453675cf7137e2368
makak22 <43416202+makak22@users.noreply.github.com>
2022-08-17 18:05:18 +0000

    23

--- public/data/ladatabase.js
+++ public/data/ladatabase.js
@@ -197,6 +197,21 @@ export const toli = [
           },
           "expression": {
             "1":"Le toli du bangando",
+ 
+            "3": null
+          },
+          "definitionEx": {
+            "1":"Franck a servit le becto à table.",
+            "2": null,
+            "3":null
+          },
+          "synonymes": {
+            "1":"Nourriture 🇫🇷, \n Bema, \n Bèlet",
+            "2": null,
+            "3": null
+          },
+          "expression": {
+            "1":"Il est midi, on mange le becto de maman",
             "2":null,
             "3":null
           }
@@ -212,6 +227,7 @@ export const toli = [
           "definition": {
             "1": "Toute substance solide qui peut être consommée par les organismes vivants, notamment en mangeant, afin d'entretenir la vie.",
             "2": null,
+ 
             "3": null
           },
           "definitionEx": {
